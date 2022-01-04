478520eb7c5413acc9991f100b1be8f222f6498b
Alex Riedler <alex@riedler.ca>
2022-01-04 09:09:49 -0500

    chore: update benchmarks to latest build of resource-struct

--- ostruct_performance.rb
+++ ostruct_performance.rb
@@ -9,15 +9,13 @@ require 'ostruct'
 require 'hashie'
 require 'hashery'
 require 'recursive_open_struct'
-require_relative 'fast_struct_read_only'
-require_relative 'firm_struct'
+require 'resource_struct'
 
 open_struct = JSON.parse(file_data, object_class: OpenStruct)
 open_cascade = Hashery::OpenCascade[data]
 recursive_struct = RecursiveOpenStruct.new(data, recurse_over_arrays: true)
 mash = Hashie::Mash.new(data)
-fast_struct = FastStructReadOnly.new(data)
-firm_struct = FirmStruct.new(data)
+firm_struct = ResourceStruct::FirmStruct.new(data)
 
 puts "### Init\n\n```"
 
@@ -37,11 +35,11 @@ Benchmark.bm do |x|
   x.report("recursive_open_struct create") do
     1000.times { RecursiveOpenStruct.new(data, recurse_over_arrays: true) }
   end
-  x.report("fast_struct_read_only create") do
-    1000.times { FastStructReadOnly.new(data) }
-  end
   x.report("firm_struct create") do
-    1000.times { FirmStruct.new(data) }
+    1000.times { ResourceStruct::FirmStruct.new(data) }
+  end
+  x.report("firm_struct create - json parse") do
+    1000.times { JSON.parse(file_data, object_class: ResourceStruct::FirmStruct) }
   end
 end
 
@@ -63,11 +61,11 @@ Benchmark.memory do |x|
   x.report("recursive_open_struct create") do
     1000.times { RecursiveOpenStruct.new(data, recurse_over_arrays: true) }
   end
-  x.report("fast_struct_read_only create") do
-    1000.times { FastStructReadOnly.new(data) }
-  end
   x.report("firm_struct create") do
-    1000.times { FirmStruct.new(data) }
+    1000.times { ResourceStruct::FirmStruct.new(data) }
+  end
+  x.report("firm_struct create - json parse") do
+    1000.times { JSON.parse(file_data, object_class: ResourceStruct::FirmStruct) }
   end
 end
 
@@ -96,12 +94,6 @@ Benchmark.bm do |x|
     5000.times do
       recursive_struct.data[0].name.firstname
       recursive_struct.data[0].friends[0].name
-    end
-  end
-  x.report("fast_struct_read_only lookup") do
-    5000.times do
-      fast_struct.data[0].name.firstname
-      fast_struct.data[0].friends[0].name
     end
   end
   x.report("firm_struct lookup") do
@@ -137,12 +129,6 @@ Benchmark.memory do |x|
     5000.times do
       recursive_struct.data[0].name.firstname
       recursive_struct.data[0].friends[0].name
-    end
-  end
-  x.report("fast_struct_read_only lookup") do
-    5000.times do
-      fast_struct.data[0].name.firstname
-      fast_struct.data[0].friends[0].name
     end
   end
   x.report("firm_struct lookup") do
@@ -176,11 +162,6 @@ Benchmark.bm do |x|
       recursive_struct.to_hash
     end
   end
-  x.report("fast_struct_read_only to hash") do
-    1000.times do
-      fast_struct.to_hash
-    end
-  end
   x.report("firm_struct to hash") do
     1000.times do
       firm_struct.to_hash
@@ -207,11 +188,6 @@ Benchmark.memory do |x|
   x.report("recursive_open_struct to_hash") do
     1000.times do
       recursive_struct.to_hash
-    end
-  end
-  x.report("fast_struct_read_only to hash") do
-    1000.times do
-      fast_struct.to_hash
     end
   end
   x.report("firm_struct to hash") do
